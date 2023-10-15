76dae19bf52798797b5094f647cbd61169a360c5
Martin Omar Demski <demski89@hotmail.com>
2023-10-15 00:46:21 -0300

    Letras y estilo

--- assets/js/menuCelular.js
+++ assets/js/menuCelular.js
@@ -2,8 +2,6 @@ const categorias = document.querySelector("#categorias");
 
 const menu = document.querySelector("#menuCel");
 
-categorias.style.display = "none"
-
 function showMenu(){
     if(categorias.style.display != "none"){
         categorias.style.display = "none";
